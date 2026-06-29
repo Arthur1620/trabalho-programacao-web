e630a100e5f7246de1847016a8325c6a899ad747
Pietro Saraiva <damatapietro@gmail.com>
2026-06-29 20:21:13 -0300

    js no index

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ function mostrarMensagem() {
   const mensagem = document.getElementById("mensagem");
   mensagem.textContent = "Obrigado por visitar nosso site!";
 }
+let ultimoScroll = 0;
+const menu = document.querySelector('.topo-site');
+
+window.addEventListener('scroll', () => {
+    const scrollAtual = window.pageYOffset;
+
+    if (scrollAtual <= 0) {
+        menu.classList.remove('escondido');
+        return;
+    }
+
+    if (scrollAtual > ultimoScroll && !menu.classList.contains('escondido') && scrollAtual > 70) {
+        menu.classList.add('escondido');
+    } 
+    else if (scrollAtual < ultimoScroll && menu.classList.contains('escondido')) {
+        menu.classList.remove('escondido');
+    }
+
+    ultimoScroll = scrollAtual;
+});
