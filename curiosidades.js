a4150108941b4ed8ca5977652b591a7a31dfdc11
Pietro Saraiva <damatapietro@gmail.com>
2026-06-29 19:26:26 -0300

    adicionei o menu com java script

---
+++ curiosidades.js
@@ -0,0 +1,20 @@
+let ultimoScroll = 0;
+const menu = document.querySelector('.topo-site');
+
+window.addEventListener('scroll', () => {
+    const scrollAtual = window.pageYOffset;
+
+    if (scrollAtual <= 0) {
+        menu.classList.remove('escondido');
+        return;
+    }
+
+    if (scrollAtual > ultimoScroll && !menu.classList.contains('escondido') && scrollAtual > 70) {
+        menu.classList.add('escondido');
+    } 
+    else if (scrollAtual < ultimoScroll && menu.classList.contains('escondido')) {
+        menu.classList.remove('escondido');
+    }
+
+    ultimoScroll = scrollAtual;
+});
